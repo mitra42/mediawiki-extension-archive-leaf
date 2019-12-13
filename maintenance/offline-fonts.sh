e198f768a704a73042b6867525016add06290613
Mitra Ardron <mitra@mitra.biz>
2019-12-13 21:08:45 +1100

    Script to install offline fonts

---
+++ maintenance/offline-fonts.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+echo "Installing mediawiki-extension-archive-leaf fonts for offline use of Palm wiki"
+# NOTE THIS SHOULD BE RUN FROM THE ARCHIVELEAF EXTENSION DIRECTORY AS "maintenance/offline-fonts.sh"
+
+# TODO INTEGRATE NOTES FROM EVERNOTE IN HERE
+#BASEMEDIAWIKI=/usr/share/mediawiki
+#ARCHIVELEAFDIR=${BASEMEDIAWIKI}/extensions/ArchiveLeaf
+BASEMEDIAWIKI="../.."
+ARCHIVELEAFDIR="."
+FONTSDIR=${ARCHIVELEAFDIR}/fonts
+FONTSURL=/mediawiki/extensions/ArchiveLeaf/fonts
+FONTSSCSS=${ARCHIVELEAFDIR}/scss/fonts-custom.scss
+set -e # End script on first error
+mkdir -p ${FONTSDIR}
+pushd ${FONTSDIR} >>/dev/null
+touch ${FONTSSCSS} # Make sure it exists
+while read -ra FONTLINE
+do
+    FONTNAME=${FONTLINE[0]}
+    FONTURL=${FONTLINE[1]}
+    FONTVER=${FONTLINE[2]}
+    for FONTEXT in .woff .woff2 .ttf
+    do
+       [ -f ${FONTNAME}${FONTEXT} ] || curl -L -o${FONTNAME}${FONTEXT} ${FONTURL}${FONTEXT}${FONTVER}
+    done
+    cp ${FONTSSCSS} /tmp/m1
+    grep -v ${FONTNAME} /tmp/m1 > ${FONTSSCSS} || echo "adding ${FONTNAME}" # So doesnt end script if fontname not there
+    echo "\$${FONTNAME}: \"${FONTSURL}/${FONTNAME}\";" >>${FONTSSCSS}
+done <<EOT
+vimala https://bali.panlex.org/transcriber/fonts/Vimala ?v22
+pustaka https://bali.panlex.org/transcriber/fonts/Pustaka ?v22
+noto_balinese https://archive.org/cors/NotoFonts/NotoSerifBalinese-Regular
+noto_grantha https://archive.org/cors/NotoFonts/NotoSansGrantha-Regular
+noto_khmer https://archive.org/cors/NotoFonts/NotoSansKhmer-Regular
+noto_malayalam https://archive.org/cors/NotoFonts/NotoSansMalayalam-Regular
+noto_myanmar https://archive.org/cors/NotoFonts/NotoSansMyanmar-Regular
+noto_sinhala https://archive.org/cors/NotoFonts/NotoSansSinhala-Regular
+noto_tamil https://archive.org/cors/NotoFonts/NotoSansTamil-Regular
+noto_telugu https://archive.org/cors/NotoFonts/NotoSansTelugu-Regular
+EOT
+popd >>/dev/null
+echo Ending font install
---
+++ maintenance/offline.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+echo "Installing mediawiki-extension-archive-leaf fonts for offline use of Palm wiki"
+set -e
+BASEMEDIAWIKI="../.."
+ARCHIVELEAFIR="."
+#BASEMEDIAWIKI=/usr/share/mediawiki
+#ARCHIVELEAFDIR=${BASEMEDIAWIKI}/extensions/ArchiveLeaf
+#TODO offline-fonts could just use the env variables
+export BASEMEDIAWIKI ARCHIVELEAFDIR
+
+# This is following the steps in README.md
+pushd ${ARCHIVELEAFDIR} >>/dev/null
+maintenance/transcriber-install # yarn dependencies
+maintenance/offline-fonts.sh # Install offline fonts
+maintenance/transcriber-build # transcriber javascript and css
+maintenance/templates-import # import the required Template:Entry and Template:EntryImage pages into MediaWiki.
+# The Metrolook skin is included here, because MediaWiki's skin distribution system makes it impossible (maybe intentionally)
+# to script fetching skins.
+# You can get the skin at https://www.mediawiki.org/wiki/Skin:Metrolook
+# and I've opened a discussion at https://www.mediawiki.org/wiki/Extension_talk:ExtensionDistributor
+# but searching some past history suggests this is one of those wicked discussions they'll never solve
+# because they've scoped the problem just too hard!
+if [ -d ${BASEMEDIAWIKI}/skins/Metrolook ]
+then
+  echo "Metrolook skin appears to be installed"
+else
+  tar -xzf Metrolook.tar.gz -C ${BASEMEDIAWIKI}/skins
+fi
+while read LOCALSETTING
+do
+    grep -v ${LOCALSETTING} ${BASEMEDIAWIKI}/LocalSettings.php || echo ${LOCALSETTING} >>${BASEMEDIAWIKI}/LocalSettings.php
+done <<EOT
+$wgAllowExternalImages = true;
+$wgAllowImageTag = true;
+$wgCapitalLinks = false;
+$wgAllowDisplayTitle = true;
+$wgRestrictDisplayTitle = false;
+$wgAllowCopyUploads = true;
+$wgEnableUploads = true;
+ini_set('max_execution_time', 0);
+# recommended (requires ImageMagick install)
+$wgUseImageMagick = true;
+# load ArchiveLeaf extension
+wfLoadExtension( 'ArchiveLeaf' );
+# use Metrolook (required for mobile)
+wfLoadSkin( 'Metrolook' );
+$wgMetrolookDownArrow = false;
+$wgMetrolookUploadButton = false;
+$wgDefaultSkin = 'Metrolook';
+EOT
+popd >>/dev/null
